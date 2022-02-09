b8d50e1f40b689a4d8ff9399f22ff90b7e4f2854
Voloseniuc Matei <volo.matei@gmail.com>
2022-02-09 20:25:24 +0200

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
 import methodOverride from 'method-override'
 import articlesRouter from './routes/article.js'
+
+dotenv.config()
 const app=express()
 const port=process.env.PORT || 3000
 
